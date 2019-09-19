38e6fb2994cc3493200bc8ca0cfaf124c832b62e
romanbr1 <romanbr@meta.ua>
2019-09-19 10:46:41 +0300

    2 files modified

--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -11,6 +11,12 @@ export class CarsComponent {
   addCarStatus = false;
   cars = ['Ford', 'Audi', 'BMW', 'Mazda', 'Lada', 'Bently'];
 
+  dates=[
+    new Date(2015,3,4),
+    new Date(2014,4,2),
+    new Date(2013,7,1),
+    new Date(2011,9,11)
+  ]
   constructor() {
   }
 
